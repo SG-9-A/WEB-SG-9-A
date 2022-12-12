15d326817e38e87edb4a867873954ba993d7d09d
kainukai <117354315+kainukai@users.noreply.github.com>
2022-12-12 14:26:50 +0200

    Lisätty kommentteja koodiin.

--- JavaScript/kainulainen.js
+++ JavaScript/kainulainen.js
@@ -1,3 +1,4 @@
+// Matikkapelin koodi, koostuu 2 random generaattorista joita käytetään laskujen luomiseen
 let n1 = Math.floor(Math.random()*10+1)
 let n2 = Math.floor(Math.random()*10+1)
 document.getElementById("v1").innerHTML = n1;
@@ -14,7 +15,7 @@ document.getElementById("message").innerHTML = "Yritä saada täydet pisteet!";
 
 let ans = n1 + n2;
 
-
+// Pelaaja saa pisteitä oikeasta vastauksesta ja sivu generoi uuden laskun. Peli päättyy viiden kysymyksen jälkeen.
 const jsGame = () => {
     var usera = document.getElementById("answ").value;
     if (usera == ans) {
@@ -26,14 +27,14 @@ const jsGame = () => {
         
     }
 
-
+// Jokaisesta väärästä kysymyksestä lähtee yksi piste pois ja peli kertoo oikean vastauksen. Et saa pistettä vaikka korjaisit oikean vastauksen jälkikäteen.
     else {    
         wrong = wrong-1;
         count++;
         document.getElementById("message").innerHTML = `Väärin! ${ans} oli oikea vastaus`
         
     }
-
+// Viiden kysymyksen jälkeen peli näyttää loppuruudun riippuen siitä montako pistettä olet saanut. Enemmän kuin kolme pistettä niin peli sanoo että voitit ja se näyttää siihen liittyvän kuvan.
     if (count == 5) {
         Visible();
         var s = score;
@@ -43,7 +44,7 @@ const jsGame = () => {
             document.getElementById("restart").style.visibility = "visible";
             document.getElementById("winner").style.visibility = "visible";
             
-
+// Jos häviät niin peli ilmoittaa senkin ja näyttää siihen erilaisen kuvan.
         }else{
             document.getElementById("answ").value = " ";
             document.getElementById("result").innerHTML = `Hävisit ja sait ${wrong}/5 oikein`
@@ -55,6 +56,7 @@ const jsGame = () => {
 
 };
 
+// Matikkapelin randomigeneraattori
 function GenerateRandom() {
     document.getElementById("answ").value = " ";
     n1 = Math.floor(Math.random() * 10 + 1)
@@ -66,7 +68,7 @@ function GenerateRandom() {
     ans = n1 + n2;
 }
 
-
+// Peli piilottaa tiettyjä sivun osia kun olet suorittanut testin.
 function Visible() {
     document.getElementById("v1").style.visibility = "hidden";
     document.getElementById("v2").style.visibility = "hidden";
